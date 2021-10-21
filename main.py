6f6b12ffa8e9a815c69d0fd3fd25845c4eb7d0d1
Auditory Prostethic Group <47893511+APGDHZ@users.noreply.github.com>
2021-10-21 22:15:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ import tensorflow as tf
 import json
 import glob
 import warnings
-import xlsxwriter
 import scipy.io as sio
 from utils import setup
 from model import DeepACE
@@ -26,7 +25,6 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from collections import namedtuple
 from data_generator import DataGenerator
-from keras.utils.layer_utils import count_params
 
 def train(args):
         
@@ -115,7 +113,6 @@ def evaluate(args, model):
     args = namedtuple("args", args.keys())(*args.values())
     
     valid_ds = DataGenerator("valid", args).fetch()
-    test_ds = DataGenerator("test", args).fetch()
     
     physical_devices = tf.config.list_physical_devices("GPU")
     
@@ -136,111 +133,9 @@ def evaluate(args, model):
     print("\nEvaluating model... \n")
 
     mse_valid = model.evaluate(valid_ds, callbacks = test_logger, verbose = 0)
-    mse_test = model.evaluate(test_ds, callbacks = test_logger, verbose = 0)
     
-    print("Evaluation MSE:", round(mse_valid[0]/2,4))
+    print("Evaluation MSE:", round(mse_valid,4))
     
-    with open(os.path.join(model_dir, "Evaluation results[" + model_time_stamp + "].txt"), "w") as f:
-        f.write("Topology: {:s}\n".format(args.topology))
-        f.write("Mean MSE: {:.4f}\n".format(mse_valid[0]/2))
-        f.write("Left MSE: {:.4f}\n".format(mse_valid[1]))
-        f.write("Right MSE: {:.4f}\n".format(mse_valid[2]))
-        f.write("Number of parameters: {:d}\n".format(count_params(model.trainable_variables)))
-    f.close()
-    
-    print("Test MSE:", round(mse_test[0]/2,4))
-    
-    with open(os.path.join(model_dir, "Test results[" + model_time_stamp + "].txt"), "w") as f:
-        f.write("Topology: {:s}\n".format(args.topology))
-        f.write("Mean MSE: {:.4f}\n".format(mse_test[0]/2))
-        f.write("Left MSE: {:.4f}\n".format(mse_test[1]))
-        f.write("Right MSE: {:.4f}\n".format(mse_test[2]))
-        f.write("Number of parameters: {:d}\n".format(count_params(model.trainable_variables)))
-    f.close()
-
-    workbook = xlsxwriter.Workbook(os.path.join(model_dir, "Evaluation results[" + model_time_stamp + "].xlsx"))
-    worksheet = workbook.add_worksheet()
-    data= (
-        ['Topology',  args.topology],
-        ['MeanMSE',   mse_valid[0]/2],
-        ['LeftMSE',   mse_valid[1]],
-        ['RightMSE',  mse_valid[2]],
-        ['Params',    count_params(model.trainable_variables)],
-        ['SkipSize',  args.S])
-    
-    row = 0
-    col = 0
-    
-    for var, value in (data):
-        worksheet.write(row, col, var)
-        worksheet.write(row, col + 1, value)
-        row += 1
-    
-    workbook.close()
-    
-    workbook = xlsxwriter.Workbook(os.path.join(model_dir, "Test results[" + model_time_stamp + "].xlsx"))
-    worksheet = workbook.add_worksheet()
-    data= (
-        ['Topology',  args.topology],
-        ['MeanMSE',   mse_test[0]/2],
-        ['LeftMSE',   mse_test[1]],
-        ['RightMSE',  mse_test[2]],
-        ['Params',    count_params(model.trainable_variables)],
-        ['SkipSize',  args.S])
-    
-    row = 0
-    col = 0
-    
-    for var, value in (data):
-        worksheet.write(row, col, var)
-        worksheet.write(row, col + 1, value)
-        row += 1
-    
-    workbook.close()
-
-def test(args, model):         
-    
-    model_time_stamp = model[-20:-1]
-   
-    model_dir = os.path.join(args.model_dir, model)
-      
-    save_path = os.path.join(model_dir, "predictions/")
-    
-    if not os.path.isdir(save_path):
-        os.mkdir(save_path) 
-    
-    params_file = os.path.join(model_dir, "params.json")
-    
-    with open(params_file) as f:
-        args = json.load(f)
-                
-    args = namedtuple("args", args.keys())(*args.values())
-    
-    test_ds = DataGenerator("test", args).fetch()
-    
-    physical_devices = tf.config.list_physical_devices("GPU")
-    
-    if not physical_devices:
-        args.GPU = False
-
-    model = DeepACE(args).call()
-    
-    model.load_weights(os.path.join(model_dir, "model_best[{}].h5".format(model_time_stamp)))
-    
-    model.compile(loss = 'MSE')
-    
-    model.summary();
-    
-    file_names = glob.glob("data/test/*.wav")
-    
-    i = 0
-    
-    for x, y in test_ds:
-        left_out, right_out = model([x, y])
-        out = tf.concat([tf.expand_dims(left_out[0], -1), tf.expand_dims(right_out[0],-1)], axis = -1)
-        sio.savemat(os.path.join(save_path + "deep_ace_" + file_names[i][10:] + ".mat"), {"out": out.numpy()})
-        i += 1
- 
 def main():
 
     args = setup()
@@ -251,14 +146,10 @@ def main():
         for tm in  trained_models:
             test(args, tm)
             
-    elif args.mode == "evaluate":
+    else args.mode == "evaluate":
         trained_models = os.listdir(args.model_dir)
         for tm in  trained_models:
             evaluate(args, tm)
-    else:
-        trained_models = os.listdir(args.model_dir)
-        for tm in  trained_models:
-            test(args, tm)
-            
+         
 if __name__ == '__main__':
-    main()  
+    main()      
